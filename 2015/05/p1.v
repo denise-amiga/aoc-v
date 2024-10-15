a3f431863f49df53878110a6e6136f2ef4996e24
Denise <denise_amiga@hotmail.com>
2024-10-15 14:25:50 +0200

    1505-1

--- 2015/05/p1.v
+++ 2015/05/p1.v
@@ -1,16 +1,55 @@
 import os
 import benchmark
 
-fn p1(txt string) int {
-    mut result := 0
-    return result
+fn check1(txt string) bool {
+	mut v := 0
+	vowels := [u8(`a`), u8(`e`), u8(`i`), u8(`o`), u8(`u`)]
+	for c in txt {
+		if c in vowels {
+			v += 1
+		}
+	}
+	return v > 2
+}
+
+fn check2(txt string) bool {
+	for i in 0 .. txt.len - 1 {
+		if txt[i] == txt[i + 1] {
+			return true
+		}
+	}
+	return false
+}
+
+fn check3(txt string) bool {
+	fails := ['ab', 'cd', 'pq', 'xy']
+	for t in fails {
+		if txt.contains(t) {
+			return false
+		}
+	}
+	return true
+}
+
+fn p1(txt string) bool {
+	return check1(txt) && check2(txt) && check3(txt)
 }
 
 fn main() {
-    assert p1('') == 0
+	assert p1('ugknbfddgicrmopn') == true
+	assert p1('aaa') == true
+	assert p1('jchzalrnumimnmhp') == false
+	assert p1('haegwjzuvuyypxyu') == false
+	assert p1('dvszwmarrgswjxmb') == false
 
-    mut b := benchmark.start()
-    ar := os.read_lines('input.txt') or { panic('No input') }
-    println(p1(ar[0]))
-    b.measure('P1')
+	mut result := 0
+	mut b := benchmark.start()
+	ar := os.read_lines('input.txt') or { panic('No input') }
+	for l in ar {
+		if p1(l) {
+			result += 1
+		}
+	}
+	println(result)
+	b.measure('P1')
 }
