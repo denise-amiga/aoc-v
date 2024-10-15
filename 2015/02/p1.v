75969f151e82646cd2ef0bf81d25c999e8f75e10
Denise <denise_amiga@hotmail.com>
2024-10-15 14:23:21 +0200

    1502-1

--- 2015/02/p1.v
+++ 2015/02/p1.v
@@ -2,15 +2,27 @@ import os
 import benchmark
 
 fn p1(txt string) int {
-    mut result := 0
-    return result
+	ar := txt.split('x').map(it.int())
+	mut a, mut b, mut c := ar[0], ar[1], ar[2]
+	if a > b {
+		a, b = b, a
+	}
+	if b > c {
+		b, c = c, b
+	}
+	return 2 * (a * b + b * c + a * c) + a * b
 }
 
 fn main() {
-    assert p1('') == 0
+	assert p1('2x3x4') == 58
+	assert p1('1x1x10') == 43
 
-    mut b := benchmark.start()
-    ar := os.read_lines('input.txt') or { panic('No input') }
-    println(p1(ar[0]))
-    b.measure('P1')
+	mut result := 0
+	mut b := benchmark.start()
+	ar := os.read_lines('input.txt') or { panic('No input') }
+	for l in ar {
+		result += p1(l)
+	}
+	println(result)
+	b.measure('P1')
 }
