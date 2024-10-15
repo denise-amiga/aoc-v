294111480454ad6b5d13f995391c0746e8535afb
Denise <denise_amiga@hotmail.com>
2024-10-15 14:24:15 +0200

    1503-1

--- 2015/03/p1.v
+++ 2015/03/p1.v
@@ -2,15 +2,32 @@ import os
 import benchmark
 
 fn p1(txt string) int {
-    mut result := 0
-    return result
+	mut x := 0
+	mut y := 0
+	mut m := map[string]int{}
+	m['(${x}, ${y})'] += 1
+	for c in txt {
+		if c == `^` {
+			y += 1
+		} else if c == `>` {
+			x += 1
+		} else if c == `v` {
+			y -= 1
+		} else if c == `<` {
+			x -= 1
+		}
+		m['(${x}, ${y})'] += 1
+	}
+	return m.len
 }
 
 fn main() {
-    assert p1('') == 0
+	assert p1('>') == 2
+	assert p1('^>v<') == 4
+	assert p1('^v^v^v^v^v') == 2
 
-    mut b := benchmark.start()
-    ar := os.read_lines('input.txt') or { panic('No input') }
-    println(p1(ar[0]))
-    b.measure('P1')
+	mut b := benchmark.start()
+	ar := os.read_lines('input.txt') or { panic('No input') }
+	println(p1(ar[0]))
+	b.measure('P1')
 }
