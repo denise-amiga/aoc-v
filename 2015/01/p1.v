5a3cebfb307f4cce17ec60e1f501a96edc160b0e
Denise <denise_amiga@hotmail.com>
2024-10-15 14:18:06 +0200

    1501-1

--- 2015/01/p1.v
+++ 2015/01/p1.v
@@ -2,15 +2,30 @@ import os
 import benchmark
 
 fn p1(txt string) int {
-    mut result := 0
-    return result
+	mut result := 0
+	for c in txt {
+		if c == `(` {
+			result += 1
+		} else if c == `)` {
+			result -= 1
+		}
+	}
+	return result
 }
 
 fn main() {
-    assert p1('') == 0
+	assert p1('(())') == 0
+	assert p1('()()') == 0
+	assert p1('(((') == 3
+	assert p1('(()(()(') == 3
+	assert p1('))(((((') == 3
+	assert p1('())') == -1
+	assert p1('))(') == -1
+	assert p1(')))') == -3
+	assert p1(')())())') == -3
 
-    mut b := benchmark.start()
-    ar := os.read_lines('input.txt') or { panic('No input') }
-    println(p1(ar[0]))
-    b.measure('P1')
+	mut b := benchmark.start()
+	ar := os.read_lines('input.txt') or { panic('No input') }
+	println(p1(ar[0]))
+	b.measure('P1')
 }
