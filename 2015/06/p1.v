c2fa6d2ef91642d67855c8274973c943f2cb421c
Denise <denise_amiga@hotmail.com>
2024-10-15 14:26:38 +0200

    1506-1

--- 2015/06/p1.v
+++ 2015/06/p1.v
@@ -1,16 +1,64 @@
 import os
 import benchmark
 
-fn p1(txt string) int {
-    mut result := 0
-    return result
+@[direct_array_access]
+fn on(mut matrix [][]bool, x int, y int, w int, h int) {
+	for yy in y .. h + 1 {
+		for xx in x .. w + 1 {
+			matrix[yy][xx] = true
+		}
+	}
 }
 
-fn main() {
-    assert p1('') == 0
+@[direct_array_access]
+fn off(mut matrix [][]bool, x int, y int, w int, h int) {
+	for yy in y .. h + 1 {
+		for xx in x .. w + 1 {
+			matrix[yy][xx] = false
+		}
+	}
+}
 
-    mut b := benchmark.start()
-    ar := os.read_lines('input.txt') or { panic('No input') }
-    println(p1(ar[0]))
-    b.measure('P1')
+@[direct_array_access]
+fn toggle(mut matrix [][]bool, x int, y int, w int, h int) {
+	for yy in y .. h + 1 {
+		for xx in x .. w + 1 {
+			if matrix[yy][xx] {
+				matrix[yy][xx] = false
+			} else {
+				matrix[yy][xx] = true
+			}
+		}
+	}
+}
+
+@[direct_array_access]
+fn p1(txt string, mut matrix [][]bool) {
+	match txt[6] {
+		`n` { on(mut matrix, ...txt[8..].replace(' through ', ',').split(',').map(it.int())) }
+		`f` { off(mut matrix, ...txt[9..].replace(' through ', ',').split(',').map(it.int())) }
+		` ` { toggle(mut matrix, ...txt[7..].replace(' through ', ',').split(',').map(it.int())) }
+		else { panic('Error') }
+	}
+}
+
+@[direct_array_access]
+fn main() {
+	mut result := 0
+	mut matrix := [][]bool{len: 1000, init: []bool{len: 1000}}
+
+	mut b := benchmark.start()
+	ar := os.read_lines('input.txt') or { panic('No input') }
+	for l in ar {
+		p1(l, mut matrix)
+	}
+	for y in 0 .. 1000 {
+		for x in 0 .. 1000 {
+			if matrix[y][x] {
+				result += 1
+			}
+		}
+	}
+	println(result)
+	b.measure('P1')
 }
