783c31afa6667932798d692ea5161d8f468cb8d8
Denise <denise_amiga@hotmail.com>
2024-10-15 14:25:00 +0200

    1504-1

--- 2015/04/p1.v
+++ 2015/04/p1.v
@@ -1,16 +1,24 @@
 import os
 import benchmark
+import crypto.md5
 
 fn p1(txt string) int {
-    mut result := 0
-    return result
+	mut result := 1
+	for {
+		if md5.hexhash('${txt}${result}').starts_with('00000') {
+			return result
+		}
+		result += 1
+	}
+	return -1
 }
 
 fn main() {
-    assert p1('') == 0
+	assert p1('abcdef') == 609043
+	assert p1('pqrstuv') == 1048970
 
-    mut b := benchmark.start()
-    ar := os.read_lines('input.txt') or { panic('No input') }
-    println(p1(ar[0]))
-    b.measure('P1')
+	mut b := benchmark.start()
+	ar := os.read_lines('input.txt') or { panic('No input') }
+	println(p1(ar[0]))
+	b.measure('P1')
 }
